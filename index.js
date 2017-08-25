1b6264ae5b7c79dee803045edcdb8247e3dbfd9f
mix irving <whimful@gmail.com>
2017-08-25 17:51:53 +1200

    use isWindows & slash modules

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { join, dirname } = require('path')
 const { assign } = Object
 const styleResolve = require('style-resolve')
 const CssUrlRegex = require('css-url-regex')
+const isWindows = require('is-windows')
+const slash = require('slash')
 
 const cssUrlRegex = CssUrlRegex()
 const dotSlashRegex = /^[\.\/]*/
@@ -14,15 +16,13 @@ function requireStyle (name) {
   const path = styleResolve.sync(name)
   var css = readFileSync(path, 'utf8')
   
-  const isWindows = /^win/.test(process.platform)
-  
   // resolve any relative paths to absolute paths
   css = css.replace(cssUrlRegex, (_, url) => {
     url = url
       .replace(quoteRegex, '')
       .replace(dotSlashRegex, match => join(dirname(path), match))
     
-    if (isWindows) url = url.replace(/\\/g, '/')
+    if (isWindows()) url = slash(url)
 
     return `url(${url})`
   })

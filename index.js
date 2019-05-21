0838d3c6df4c67de59b1906502dea7f7e9bf89af
Christian Bundy <christianbundy@fraction.io>
2019-05-20 20:02:58 -0700

    Upgrade static-module and pass linter

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { readFileSync } = require('fs')
 const { join, dirname } = require('path')
-const { assign } = Object
 const styleResolve = require('style-resolve')
 const CssUrlRegex = require('css-url-regex')
 const isWindows = require('is-windows')
 const slash = require('slash')
 
 const cssUrlRegex = CssUrlRegex()
-const dotSlashRegex = /^[\.\/]*/
+const dotSlashRegex = /^[./]*/
 const quoteRegex = /["']*/g
 
 module.exports = requireStyle
@@ -15,13 +14,13 @@ module.exports = requireStyle
 function requireStyle (name) {
   const path = styleResolve.sync(name)
   var css = readFileSync(path, 'utf8')
-  
+
   // resolve any relative paths to absolute paths
   css = css.replace(cssUrlRegex, (_, url) => {
     url = url
       .replace(quoteRegex, '')
       .replace(dotSlashRegex, match => join(dirname(path), match))
-    
+
     if (isWindows()) url = slash(url)
 
     return `url(${url})`

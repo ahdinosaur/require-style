488d24de38a891cf5682e93eaea3c1ea58e29a43
Michael Williams <michael.williams@enspiral.com>
2019-06-08 22:36:45 +1200

    pass down caller path to resolve

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+'use strict' // needed for caller-path
+
 const { readFileSync } = require('fs')
 const { join, dirname } = require('path')
 const styleResolve = require('style-resolve')
 const isWindows = require('is-windows')
 const slash = require('slash')
+const caller = require('caller-path')
 
 const cssUrlRegex = require('./util/css-url-regex')
 const dotSlashRegex = /^[./]*/
@@ -10,7 +13,9 @@ const dotSlashRegex = /^[./]*/
 module.exports = requireStyle
 
 function requireStyle (name) {
-  const path = styleResolve.sync(name)
+  const path = styleResolve.sync(name, {
+    basedir: dirname(caller())
+  })
   var css = readFileSync(path, 'utf8')
 
   // resolve any relative paths to absolute paths

e0b5d42532a9cc473b784752da79de7140563f6e
Christian Bundy <christianbundy@fraction.io>
2019-11-14 18:27:29 -0800

    Add test to ensure test file can require itself (#8)

--- test/index.js
+++ test/index.js
@@ -4,5 +4,11 @@ const requireStyle = require('../')
 
 test('require-style', function (t) {
   t.ok(requireStyle, 'module is require-able')
+  t.ok(requireStyle('../index.js'))
+  t.end()
+})
+
+test('self', function (t) {
+  t.ok(requireStyle('./index.js'))
   t.end()
 })
